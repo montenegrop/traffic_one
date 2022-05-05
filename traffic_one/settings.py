0347422e7875ec3cf2b5ad7ef10a68e8e16d842f
montenegrop <pablo.rpm@gmail.com>
2022-05-05 14:28:23 -0300

    blog

--- traffic_one/settings.py
+++ traffic_one/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
 
     # mias
     'user',
+    'blog',
+    'blog_api'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +60,7 @@ ROOT_URLCONF = 'traffic_one.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
